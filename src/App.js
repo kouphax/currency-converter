5d9e3e27bf39c527be06b297356a6ff384c109e4
James Hughes <jamhughes@deloitte.co.uk>
2021-02-06 21:46:56 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const rates = {
+  "GBP": 1,
+  "EUR": 1.14,
+  "USD": 1.37
+}
+
+function convert(value, from, to) {
+  return ((parseFloat(value)/rates[from]) * rates[to]).toFixed(2);
+}
 
 function App() {
+
+  const [currency, setCurrency] = useState("GBP")
+  const [value, setValue] = useState("0.00")
+
+  const [gbpValue, setGbpValue] = useState("0.00")
+  const [eurValue, setEurValue] = useState("0.00")
+  const [usdValue, setUsdValue] = useState("0.00")
+
+
+  const onCurrencyChange = e => {
+    console.log(e.target.value);
+    setCurrency(e.target.value);
+    setGbpValue(convert(value, e.target.value,"GBP"))
+    setEurValue(convert(value, e.target.value,"EUR"))
+    setUsdValue(convert(value, e.target.value, "USD"))
+  }
+
+
+  const onValueChange = e => {
+    console.log(e.target.value);
+    setValue(e.target.value);
+    setGbpValue(convert(e.target.value, currency,"GBP"))
+    setEurValue(convert(e.target.value, currency,"EUR"))
+    setUsdValue(convert(e.target.value, currency, "USD"))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Input</h1>
+      <div>
+        <select onChange={onCurrencyChange} value={currency}>
+          <option value="GBP">&pound;</option>
+          <option value="EUR" >&euro;</option>
+          <option value="USD" >&#36;</option>
+        </select>
+        <input type="text" value={value} onChange={onValueChange}/>
+      </div>
+      <h1>Output</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>&pound;</td><td>{gbpValue}</td>
+          </tr>
+          <tr>
+            <td>&euro;</td><td>{eurValue}</td>
+          </tr>
+          <tr>
+            <td>&#36;</td><td>{usdValue}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
